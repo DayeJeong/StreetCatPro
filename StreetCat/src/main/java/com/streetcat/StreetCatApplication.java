91c87537d5e0c6c60fa89681bca76ace1dc20f6e
tiiwin1318 <tiiwin1318@gmail.com>
2025-07-20 18:20:43 +0900

    Project Start!!

---
+++ StreetCat/src/main/java/com/streetcat/StreetCatApplication.java
@@ -0,0 +1,13 @@
+package com.streetcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetCatApplication.class, args);
+	}
+
+}
---
+++ StreetCat/src/test/java/com/streetcat/StreetCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streetcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
